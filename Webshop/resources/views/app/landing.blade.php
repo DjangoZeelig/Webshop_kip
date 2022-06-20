79fdae1e3b8e2f750f5bc7b22f2c94bd23d2cab7
Django Zeelig <100681012+DjangoZeelig@users.noreply.github.com>
2022-06-20 10:46:17 +0200

    added marte changes

--- Webshop/resources/views/app/landing.blade.php
+++ Webshop/resources/views/app/landing.blade.php
@@ -1,3 +1,36 @@
-@foreach($products as $product)
-{{ $product->name }}
-@endforeach
+@extends('layouts.master')
+@section('content')
+<ul class="cards">
+    @foreach($products as $product)
+    <li class="cards__item">
+        <div class="card">
+            <div class="card__image card__image--a"></div>
+            <div class="card__content">
+                <div class="card__title">Kippenhok A</div>
+                <p class="card__text">{{ $product->name }}</p>
+                <button class="btn btn--block card__btn">Button</button>
+            </div>
+        </div>
+    </li>
+    <li class="cards__item">
+        <div class="card">
+            <div class="card__image card__image--b"></div>
+            <div class="card__content">
+                <div class="card__title">Kippenhok B</div>
+                <p class="card__text">{{ $product->name }}</p>
+                <button class="btn btn--block card__btn">Button</button>
+            </div>
+        </div>
+    </li>
+    <li class="cards__item">
+        <div class="card">
+            <div class="card__image card__image--c"></div>
+            <div class="card__content">
+                <div class="card__title">Kippenhok C</div>
+                <p class="card__text">{{ $product->name }}</p>
+                <button class="btn btn--block card__btn">Button</button>
+            </div>
+            @endforeach
+        </div>
+    </li>
+    @endsection
